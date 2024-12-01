27872d1620c9edcf41ceeefc3abd0be3a07895a5
Yassine Meliani <idriss.meliani@gmail.com>
2024-12-01 12:57:06 -0500

    initial commit of graphs

---
+++ src/components/Graphs/Probability/Probability.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./Probability.module.css";
+
+const Probability = () => {
+  return <div></div>;
+};
+
+
+export default Probability;
---
+++ src/components/Graphs/RSSEvolution/RSSEvolution.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './RSSEvolution.module.css';
+
+
+const RSSEvolution = () => {
+    
+
+    return (
+        <div>
+        </div>
+    );
+}
+
+export default RSSEvolution;
