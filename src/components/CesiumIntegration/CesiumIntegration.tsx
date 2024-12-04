0336761219712f74d7a3c097c9e6871527fffe59
PetrescuRad <radu.sebas.petrescu@gmail.com>
2024-12-03 21:46:21 -0500

    minor path adjustments for satellites

--- src/components/CesiumIntegration/CesiumIntegration.tsx
+++ src/components/CesiumIntegration/CesiumIntegration.tsx
@@ -12,6 +12,8 @@ const fetchSatellite1Data = async () => [
   { time: "2024-12-01T02:00:00Z", longitude: 45, latitude: 15, altitude: 500 },
   { time: "2024-12-01T02:30:00Z", longitude: 75, latitude: 5, altitude: 500 },
   { time: "2024-12-01T03:00:00Z", longitude: 105, latitude: -5, altitude: 500 },
+  { time: "2024-12-01T03:30:00Z", longitude: 125, latitude: -15, altitude: 500 },
+  { time: "2024-12-01T04:00:00Z", longitude: 150, latitude: -5, altitude: 500 },
 ];
 
 const fetchSatellite2Data = async () => [
@@ -22,6 +24,8 @@ const fetchSatellite2Data = async () => [
   { time: "2024-12-01T02:00:00Z", longitude: -120, latitude: 10, altitude: 400 },
   { time: "2024-12-01T02:30:00Z", longitude: -90, latitude: 15, altitude: 400 },
   { time: "2024-12-01T03:00:00Z", longitude: -60, latitude: 20, altitude: 400 },
+  { time: "2024-12-01T03:30:00Z", longitude: -60, latitude: 20, altitude: 400 },
+  { time: "2024-12-01T04:00:00Z", longitude: -60, latitude: 20, altitude: 400 },
 ];
 
 const CesiumIntegration: React.FC = () => {
