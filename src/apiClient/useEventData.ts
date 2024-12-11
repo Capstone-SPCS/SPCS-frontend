148cc7a1fd300d6698b445c44316b77f3856f004
Shady Guindi <shadyguindi13@gmail.com>
2024-12-10 19:27:21 -0500

    augment useEventData hook

--- src/apiClient/useEventData.ts
+++ src/apiClient/useEventData.ts
@@ -1,24 +1,50 @@
+import { useEffect, useState } from 'react';
 import useQuery from './useQuery';
 
-const useEventData = (id: string) => {
-  const query = `  
-      query GetEventData($id: ID!) {  
-        eventData(id: $id) {  
-          collision_probability  
-          miss_distance  
-          sat1_cn_n  
-          sat1_cr_r  
-          sat1_ct_t  
-          sat2_cn_n  
-          sat2_cr_r  
-          sat2_ct_t  
-        }  
-      }  
-    `;
+const GET_EVENTS_QUERY = `  
+query GetEventData($id: ID!) {  
+  eventData(id: $id) {  
+    collision_probability  
+    miss_distance  
+    sat1_cn_n  
+    sat1_cr_r  
+    sat1_ct_t  
+    sat2_cn_n  
+    sat2_cr_r  
+    sat2_ct_t  
+  }  
+}  
+`;
 
-  return useQuery({
-    query
+const useEventData = (id: string) => {
+  const [event, setEvent] = useState();
+  const [loading, setLoading] = useState(true);
+  const [eventError, setEventError] = useState();
+
+  const { data, error, fetchData } = useQuery({
+    query: GET_EVENTS_QUERY
   });
+
+  useEffect(() => {
+    if (data) {
+      setEvent(data.events)
+    }
+  }, [data])
+
+  const fetchEvent = async () => {
+    try {
+      await fetchData(); // Execute the query
+      if (error) {
+        throw new Error(error as unknown as string);
+      }
+    } catch (err: any) {
+      setEventError(err); // Capture any error during the fetch
+    } finally {
+      setLoading(false); // Set loading to false once finished
+    }
+  };
+
+  return { event, loading, eventError, fetchEvent }; // Expose the state and data for use in components
 };
 
 export default useEventData;
