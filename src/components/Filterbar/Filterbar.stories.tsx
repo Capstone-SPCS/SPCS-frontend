577c4d6bb8f813a4d9174b2a122c8ef778cef254
PetrescuRad <radu.petrescu@mail.mcgill.ca>
2025-02-09 17:10:02 -0500

    Initial filterbar component

---
+++ src/components/Filterbar/Filterbar.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';  
+import { Meta } from '@storybook/react';  
+import Filterbar from './Filterbar';  
+
+const meta: Meta<typeof Filterbar> = {  
+  title: 'Filter/Filterbar',  
+  component: Filterbar,  
+};  
+
+function Default() {  
+  return <Filterbar />;  
+}  
+
+Default.storyName = 'Default View';  
+
+export { meta as default, Default };
